b8fbaf671a8d70bceb3a2b9832add6d1e6bb7fe1
Bruno Abreu <vazs.bruno@gmail.com>
2026-03-01 10:51:26 -0300

    feat: implementa biscoito da sorte com react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './estilo.css';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    this.state={
+      textoFrase: ''
+    };
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = ['Todas as coisas são difíceis antes de se tornarem fáceis','Você sempre será a sua melhor companhia','Deixe de lado as preocupações e seja feliz','A nossa vida tem 4 sentidos… Amar, Sofrer, Lutar e Vencer. Ame muito, sofra pouco, lute bastante e vença sempre!','Amizade e Amor são coisas que se unem num piscar de olhos','Surpreender e ser surpreendido é o segredo do amor','Você é do tamanho do seu sonho','Seja gentil com os outros seres humanos','A vida coloca em nossa frente opções','A maior barreira para o sucesso é o medo do fracasso','Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.','A vida é melhor para aqueles que fazem o melhor dela','Não importa o quão devagar você vá, desde que você não pare','O sucesso é ir de fracasso em fracasso sem perder entusiasmo','Acredite em milagres, mas não dependa deles','O insucesso é apenas uma oportunidade para recomeçar com mais inteligência','O sucesso é a soma de pequenos esforços repetidos dia após dia','A vida é 10% o que acontece comigo e 90% como eu reajo a isso','A vida é uma aventura ousada ou não é nada','O segredo do sucesso é a constância do propósito'];}
+
+    quebraBiscoito(){
+      let state = this.state;
+      let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+      state.textoFrase = '" ' + this.frases[numeroAleatorio] + ' "';
+      this.setState(state);
+    }
+  render(){
+    return(
+      <div className="container">
+        <img src={require('./assets/biscoito.png')} className="img"></img>
+        <Botao nome="Abrir Biscoito" acaoBtn={this.quebraBiscoito}/>
+        <h3 className="textoFrase">{this.state.textoFrase}</h3>
+      </div>
+    );
+  }
 }
 
-export default App;
+class Botao extends Component {
+  render(){
+    return(
+      <div>
+        <button onClick={this.props.acaoBtn}>Abrir Biscoito</button>
+      </div>
+    );
+  }
+}
+export default App; 
